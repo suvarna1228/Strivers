5a30c34204bf4a4406843d9f415ea98040b36356
suvarna1405 <suvarna7404@gmail.com>
2025-02-18 01:05:34 +0530

    selection sort

---
+++ basics/recursion/fibociirec.py
@@ -0,0 +1,13 @@
+def fibonacci(n):
+    if n <= 0:
+        return "Input should be a positive integer"
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+# Example usage
+print(fibonacci(6))  # Output: 5
+print(fibonacci(10)) # Output: 34
