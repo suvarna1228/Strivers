6bc6022708389df1a576ecc68208ed5e417157f6
suvarna1405 <suvarna7404@gmail.com>
2025-07-29 22:42:30 +0530

    l

---
+++ basics/1st/mark.py
@@ -0,0 +1,12 @@
+class Solution:
+    def studentGrade(self, marks):
+        if marks >= 90:
+            print("Grade A")
+        elif marks >= 70:
+            print("Grade B")
+        elif marks >= 50:
+            print("Grade A")
+        elif marks >= 35:
+            print("Grade D")
+        else:
+            print("Fail")
