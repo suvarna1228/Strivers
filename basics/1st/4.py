21a7fd4a239ffbc2b987afaf82766ae8dc3c07e0
suvarna1405 <suvarna7404@gmail.com>
2025-07-31 16:53:05 +0530

    switch

---
+++ basics/1st/4.py
@@ -0,0 +1,16 @@
+class Solution:
+    def whichWeekDay(self, day):
+        if day== 1:
+            print("Monday")
+        elif day== 2:
+            print("Tusday")
+        elif day== 3:
+            print("Wednesday")
+        elif day==4:
+            print("Thursday")
+        elif day==5:
+            print("Friday")
+        elif day==6:
+            print("Saturday")
+        else:
+            print("Sunday")
